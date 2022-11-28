37928776fd7ae5a9459c14986217a6dedf52c111
pcjo1202 <73429928+pcjo1202@users.noreply.github.com>
2022-11-28 11:21:08 +0900

    modify

--- createClass.php
+++ createClass.php
@@ -1,4 +1,15 @@
 <?php
+
+  // function getMainCategory(){ //main category 반환
+    
+  //   return $main
+  // }
+
+  // function getSubCategory(){
+  //   return $sub
+  // }
+
+
   header("Content-Type:text/html;charset=utf-8");
   
   $dbname = 'happyTogther';
@@ -6,8 +17,7 @@
   $user = 'remoteJO';
   $password = 'remoteJO';
 
-  $connect = mysqli_connect($serverName, $user, $password);
-  mysqli_select_db($connect, $dbname);
+  $connect = mysqli_connect($serverName, $user, $password, $dbname);
 
   $title = $_POST['title'];
   $mainCategory = $_POST['mainCategory'];
@@ -17,16 +27,16 @@
   $date = $_POST['date'];
   $contents = $_POST['contents'];
 
-  echo "$title, $date, $mainCategory, $subCategory, $memberCount, $place, $datem, $contents";
+  echo "$title, $date, $mainCategory, $subCategory, $memberCount, $place, $date, $contents";
 
-  $query = "insert into 000 values('$title','$date', '$mainCategory', '$subCategory', '$memberCount', '$place', '$datem', '$contents')";
+  $query = "insert into 000 values('$title','$date', '$mainCategory', '$subCategory', '$memberCount', '$place', '$contents')";
 
-  $result = mysqli_query($connect, $query);
+  // $result = mysqli_query($connect, $query);
 
-  if(!$result){
-    die("서버와 연결 실패" . mysqli_connect_error());
-  }
-  echo " 서버 연결 성공!";
+  // if(!$result){
+  //   die("서버와 연결 실패" . mysqli_connect_error());
+  // }
+  // echo " 서버 연결 성공!";
 
   mysqli_close($connect);
 ?>
