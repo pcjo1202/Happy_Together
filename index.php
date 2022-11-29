54b744fbe062140d3644fe777bff1b896a5c04c2
pcjo1202 <73429928+pcjo1202@users.noreply.github.com>
2022-11-29 11:23:43 +0900

    수정

--- index.php
+++ index.php
@@ -5,112 +5,133 @@
   <meta charset="UTF-8">
   <title>Document</title>
   <style>
-    * {
-      font-size: 20px;
-      margin: 0;
-      padding: 0;
-      box-sizing: border-box;
-    }
-    a {
-      text-decoration: none;
-    }
-    body {
-      width: 100%;
-      height: 100vh;
-    }
+  * {
+    font-size: 20px;
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
 
-    .container {
-      height: 100%;
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      align-items: center;
-      gap: 2rem;
-    }
+  a {
+    text-decoration: none;
+  }
 
-    header>h1 {
-      font-size: 4rem;
-    }
-    .loginBox {
-      margin-top : 40px;
-      width :200px;
-      height : 50px;
-      text-align : center;
-    }
-    .loginBox a {
-      color : blue;
-    }
-    .a_login:hover, .a_join:hover {
-      color: black;
-      font-weight : bold;
-      font-size : 1.05rem;
-    }
-    /* .loginBox {
+  body {
+    width: 100%;
+    height: 100vh;
+  }
+
+  .container {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 2rem;
+  }
+
+  header>h1 {
+    font-size: 4rem;
+  }
+
+  .loginBox {
+    margin-top: 40px;
+    width: 200px;
+    height: 50px;
+    text-align: center;
+  }
+
+  .loginBox a {
+    color: blue;
+  }
+
+  .a_login:hover,
+  .a_join:hover {
+    color: black;
+    font-weight: bold;
+    font-size: 1.05rem;
+  }
+
+  /* .loginBox {
       width : 100px;
       height : 40px;
       line-height : 30px;
       text-align: center;
       border: 4px solid black;
     } */
-    ul {
-      list-style-type: none;
-      display: flex;
-      gap: 2rem;
-    }
-    button {
-      padding: 10px 20px;
-      font-size: 1.5rem;
-      cursor: pointer;
-    }
-    .cate0:hover, .cate1:hover, .cate2:hover, .cate3:hover {
-      font-size: 1.58rem;
-      color: blue;
-    }
-    .span_id{
-      color : blue;
-      font-weight : bold;
-      font-size : 1.08rem;
-    }
-    .a_logout {
-      text-decoration : none;
-      color : red;
-      margin-left : 45px;
-    }
-    .a_myInfo:hover, .a_logout:hover {
-      font-size : 1.03rem;
-      font-weight : bold;
-    }
+  ul {
+    list-style-type: none;
+    display: flex;
+    gap: 2rem;
+  }
+
+  button {
+    padding: 10px 20px;
+    font-size: 1.5rem;
+    cursor: pointer;
+  }
+
+  .cate0:hover,
+  .cate1:hover,
+  .cate2:hover,
+  .cate3:hover {
+    font-size: 1.58rem;
+    color: blue;
+  }
+
+  .span_id {
+    color: blue;
+    font-weight: bold;
+    font-size: 1.08rem;
+  }
+
+  .a_logout {
+    text-decoration: none;
+    color: red;
+    margin-left: 45px;
+  }
+
+  .a_myInfo:hover,
+  .a_logout:hover {
+    font-size: 1.03rem;
+    font-weight: bold;
+  }
   </style>
   <script>
-    document.addEventListener("DOMContentLoaded", function(){
-            let cate0 = document.querySelector(".cate0");
-            let cate1 = document.querySelector(".cate1");
-            let cate2 = document.querySelector(".cate2");
-            let cate3 = document.querySelector(".cate3");
-            let cate0Value = cate0.innerText;
-            let cate1Value = cate1.innerText;
-            let cate2Value = cate2.innerText;
-            let cate3Value = cate3.innerText;
-            cate0.addEventListener("click", function(){
-              location = "board.php?main_category_name="+cate0Value;
-            })
-            cate1.addEventListener("click", function(){
-              location = "board.php?main_category_name="+cate1Value;
-            })
-            cate2.addEventListener("click", function(){
-              location = "board.php?main_category_name="+cate2Value;
-            })
-            cate3.addEventListener("click", function(){
-              location = "board.php?main_category_name="+cate3Value;
-            })
-        });
-
+  document.addEventListener("DOMContentLoaded", function() {
+    let cate0 = document.querySelector(".cate0");
+    let cate1 = document.querySelector(".cate1");
+    let cate2 = document.querySelector(".cate2");
+    let cate3 = document.querySelector(".cate3");
+    let cate0Value = cate0.innerText;
+    let cate1Value = cate1.innerText;
+    let cate2Value = cate2.innerText;
+    let cate3Value = cate3.innerText;
+    cate0.addEventListener("click", function() {
+      location = "board.php?main_category_name=" + cate0Value;
+    })
+    cate1.addEventListener("click", function() {
+      location = "board.php?main_category_name=" + cate1Value;
+    })
+    cate2.addEventListener("click", function() {
+      location = "board.php?main_category_name=" + cate2Value;
+    })
+    cate3.addEventListener("click", function() {
+      location = "board.php?main_category_name=" + cate3Value;
+    })
+  });
   </script>
 </head>
 
 <body>
   <?php  
-    $connection = mysqli_connect('localhost','happy','together','happytogether');
+    // $connection = mysqli_connect('localhost','happy','together','happytogether');
+    $dbname = 'happyTogether';
+    $host = '52.78.0.158:56946';
+    $dbuser = 'remoteJO';
+    $dbpassword = 'remoteJO';
+    
+     $connection = mysqli_connect($host, $dbuser, $dbpassword,$dbname);
 
     $query = "select main_category_name from main_category";
     $query2 = "select count(*) from main_category";
@@ -130,7 +151,7 @@
           <button class='cate$i'>$category[0]</button>
         </li>";
         $category = mysqli_fetch_array($result);
-      }?>      
+      }?>
     </ul>
     <?php  
       if(!isset($_SESSION['id'])){
@@ -145,7 +166,7 @@
         echo "<a href='logout.php' class='a_logout'>로그아웃</a>";
       }
     ?>
-    
+
 </body>
 
 </html>
