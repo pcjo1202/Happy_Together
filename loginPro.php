5a340161706c53b8336d69787f7e4f60b759b48c
pcjo1202 <73429928+pcjo1202@users.noreply.github.com>
2022-12-01 21:10:29 +0900

    DB 호스트 수정

--- loginPro.php
+++ loginPro.php
@@ -1,28 +1,28 @@
 <?php
- header("Content-Type:text/html;charset=utf-8");
+header("Content-Type:text/html;charset=utf-8");
 
- $connection = mysqli_connect('localhost','happy','together','happytogether');
+//  $connection = mysqli_connect('localhost','happy','together','happytogether');
+$connection = mysqli_connect('52.78.0.158:56946', 'remoteJO', 'remoteJO', 'happyTogether');
 
-  $id = $_POST['id'];
-  $password = $_POST['password'];
-  $query = "select id, password, name from member where id='$id'";
+$id = $_POST['id'];
+$password = $_POST['password'];
+$query = "select id, password, name from member where id='$id'";
 
-  $result = mysqli_query($connection, $query);
-  $loginPro = mysqli_fetch_array($result); 
+$result = mysqli_query($connection, $query);
+$loginPro = mysqli_fetch_array($result);
 
-  if($loginPro[0]) {
-    if("$password" == "$loginPro[1]"){
-      echo "<script> 
+if ($loginPro[0]) {
+  if ("$password" == "$loginPro[1]") {
+    echo "<script> 
         alert('로그인 성공'); 
         location.href='index.php';
       </script>";
-      session_start();
-      $_SESSION['id'] = $id;
-      $_SESSION['name'] = $loginPro[2];
-    }
-    else {
-      echo "<script>alert('비밀번호가 다릅니다.'); history.back();</script>";
-    }
-  }else {
-    echo "<script>alert('아이디가 다릅니다.'); history.back();</script>";
+    session_start();
+    $_SESSION['id'] = $id;
+    $_SESSION['name'] = $loginPro[2];
+  } else {
+    echo "<script>alert('비밀번호가 다릅니다.'); history.back();</script>";
   }
+} else {
+  echo "<script>alert('아이디가 다릅니다.'); history.back();</script>";
+}
