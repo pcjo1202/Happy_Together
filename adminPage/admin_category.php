47e09f3978b52b977c58fdf47f677a941bb9586a
pcjo1202 <73429928+pcjo1202@users.noreply.github.com>
2022-12-18 13:51:34 +0900

    이미지 변경

--- adminPage/admin_category.php
+++ adminPage/admin_category.php
@@ -3,109 +3,128 @@ header("refresh: 2;");?>
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
+  <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>카테고리 페이지</title>
     <style type="text/css">
-        .container {
-        display: flex; }
-        .left{width: 400px;  margin: auto 0;}
-        .center{flex:3; text-align: center;}
-        .right{width:400px; text-align:right; margin: auto 0;}
+    .container {
+      display: flex;
+    }
 
-        a {
-            text-decoration: none;
-            color: rgb(0, 132, 255);
-            font-size: 40px;
-            align: center;
-        }
-        a:hover{color:rgb(255, 153, 0);}
+    .left {
+      width: 400px;
+      margin: auto 0;
+    }
 
-        body {
-            font-size: 16px;
-            background:#EEFFF1;
-        }
+    .center {
+      flex: 3;
+      text-align: center;
+    }
 
-        .title {
-            border-top: 3px solid #999;
-            border-bottom: 2px solid #999;
-            background: #eee;
-            font-weight: bold
-        }
+    .right {
+      width: 400px;
+      text-align: right;
+      margin: auto 0;
+    }
 
-        h1 {
-            color: rgb(0, 132, 255)
-        }
+    a {
+      text-decoration: none;
+      color: rgb(0, 132, 255);
+      font-size: 40px;
+      align: center;
+    }
 
-        .category {
-            margin-right: 10%;
-            margin-left: 10%;
-            background-color: white;
-            border: 4px double green;
-            padding: 10px;
-        }
+    a:hover {
+      color: rgb(255, 153, 0);
+    }
 
-        button {
-            height: 50px;
-            margin-left: 30px;
-            font-size: 25px;
-            background-color: #ff9c57;
-            border: none;
-            color: white;
-        }
+    body {
+      font-size: 16px;
+      background: #EEFFF1;
+    }
 
-        h1 {
-            text-align: center
-        }
+    .title {
+      border-top: 3px solid #999;
+      border-bottom: 2px solid #999;
+      background: #eee;
+      font-weight: bold
+    }
 
-        p {
-            text-align: center;
-            font-size: 50px;
-            color: #ff9c57;
-        }
+    h1 {
+      color: rgb(0, 132, 255)
+    }
 
-        .plus {
-            img src="image/plus.png" width="50" height="60"
-        }
-        .plus_image {
-            float: right;
-            margin-right : 20px;
-            cursor: pointer;
-        }
-        hr {
-            height: 2px;
-            background-color: #1AAB8A;
-        }
+    .category {
+      margin-right: 10%;
+      margin-left: 10%;
+      background-color: white;
+      border: 4px double green;
+      padding: 10px;
+    }
+
+    button {
+      height: 50px;
+      margin-left: 30px;
+      font-size: 25px;
+      background-color: #ff9c57;
+      border: none;
+      color: white;
+    }
+
+    h1 {
+      text-align: center
+    }
+
+    p {
+      text-align: center;
+      font-size: 50px;
+      color: #ff9c57;
+    }
+
+    .plus {
+      /* img src="image/plus.png" width="50" height="60" */
+    }
+
+    .plus_image {
+      float: right;
+      margin-right: 20px;
+      cursor: pointer;
+    }
+
+    hr {
+      height: 2px;
+      background-color: #1AAB8A;
+    }
     </style>
     <script>
-        document.addEventListener("DOMContentLoaded", function() {
-            let main_category_image = document.querySelector(".main_category_image");
-            let sub_category_image = document.querySelector(".sub_category_image");
+    document.addEventListener("DOMContentLoaded", function() {
+      let main_category_image = document.querySelector(".main_category_image");
+      let sub_category_image = document.querySelector(".sub_category_image");
 
-            main_category_image.addEventListener("click",function() {
-                window.open(
-                    "maincate_pop.html",
-                    "Child",
-                    "width=800, height=500, top=200, left=320"
-                );
+      main_category_image.addEventListener("click", function() {
+        window.open(
+          "maincate_pop.html",
+          "Child",
+          "width=800, height=500, top=200, left=320"
+        );
 
-            })
-            sub_category_image.addEventListener("click",function() {
-                window.open(
-                    "subcate_pop.php",
-                    "Child",
-                    "width=800, height=500, top=200, left=320"
-                );
+      })
+      sub_category_image.addEventListener("click", function() {
+        window.open(
+          "subcate_pop.php",
+          "Child",
+          "width=800, height=500, top=200, left=320"
+        );
 
-            })
+      })
 
-        })
+    })
     </script>
-</head>
+  </head>
 
-<body>
+  <body>
     <?php 
     
          $connection = mysqli_connect('52.78.0.158','remoteJO','remoteJO','happyTogether',56946);        
@@ -129,34 +148,36 @@ header("refresh: 2;");?>
         $sub_category_count = mysqli_num_rows($sub_category_result);
         
     ?>
-<div class="container">
-    <div class="left">   </div>
-    <div class="center"><a href="adminMain.php">관리자 페이지 </a> <br> <h2>카테고리</h2> </div>
-    <div class="right"> </div>
-</div>  
-<hr>
+    <div class="container">
+      <div class="left"> </div>
+      <div class="center"><a href="adminMain.php">관리자 페이지 </a> <br>
+        <h2>카테고리</h2>
+      </div>
+      <div class="right"> </div>
+    </div>
+    <hr>
     <p> 메인 카테고리 </p>
     <div class="category">
-        <?php
+      <?php
             for($i=0; $i < $main_category_count; $i++){
                 echo "<button>$main_category[0]</button>";
                 $main_category = mysqli_fetch_array($main_category_result);
             }
         ?>
-        <img src="image/plus.png" class="plus_image main_category_image" width="48" height="48">
+      <img src="../image/plus.png" class="plus_image main_category_image" width="48" height="48">
     </div>
     <br>
     <p> 서브 카테고리 </p>
     <div class="category">
-        <?php
+      <?php
             for($i=0; $i < $sub_category_count; $i++){
                 echo "<button>$sub_category[0]</button>";
                 $sub_category = mysqli_fetch_array($sub_category_result);
             }
         ?>
-        <img src="image/plus.png" class="plus_image sub_category_image" width="48" height="48">
+      <img src="../image/plus.png" class="plus_image sub_category_image" width="48" height="48">
     </div>
 
-</body>
+  </body>
 
 </html>
--- adminPage/subcate_pop.php
+++ adminPage/subcate_pop.php
@@ -1,62 +1,73 @@
 <html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-        <title>서브카테고리</title>
-        <style>
-        input,
-        select { border: 1;}
-        .button{
-        background-color:#CCEEFF;
-        border: 1;
-        border-color:#CCEEFF;
-        width:100px;
-        margin:20px;
-        }
-            .button{
-        background-color:#CCEEFF;
-        border: 1;
-        border-color:#CCEEFF;
+  <html lang="en">
 
-        }
-        .textarea {
+    <head>
+      <meta charset="UTF-8">
+      <title>서브카테고리</title>
+      <style>
+      input,
+      select {
+        border: 1;
+      }
+
+      .button {
+        background-color: #CCEEFF;
+        border: 1;
+        border-color: #CCEEFF;
+        width: 100px;
+        margin: 20px;
+      }
+
+      .button {
+        background-color: #CCEEFF;
+        border: 1;
+        border-color: #CCEEFF;
+
+      }
+
+      .textarea {
         background-color: #EEFBFF;
         border: 1;
         border-color: #CCEEFF;
         width: 150px;
         margin: 10px;
-        }
-        .mainmessage{
-            font-size:50px;
-            text-align: center;
-            font-weight: 600;
-            margin:50px;
-        }
-        .message{
-        font-weight: 800; 
-        font-size: 18px; 
+      }
+
+      .mainmessage {
+        font-size: 50px;
+        text-align: center;
+        font-weight: 600;
+        margin: 50px;
+      }
+
+      .message {
+        font-weight: 800;
+        font-size: 18px;
         color: rgb(0, 6, 128);
         margin: 30px;
-        }     
-        .container {
+      }
+
+      .container {
         text-align: center;
-        }
-        .btns {
-            margin-top : 40px;
-            text-align : center;
-        }
-        </style>
-        <script>
-            document.addEventListener("DOMContentLoaded", function(){
-                let close = document.querySelector(".close");
-                close.addEventListener("click", function() {
-                    window.close();
-            })
-    })
-        </script>
+      }
+
+      .btns {
+        margin-top: 40px;
+        text-align: center;
+      }
+      </style>
+      <script>
+      document.addEventListener("DOMContentLoaded", function() {
+        let close = document.querySelector(".close");
+        close.addEventListener("click", function() {
+          window.close();
+        })
+      })
+      </script>
     </head>
+
     <body>
-        <?php 
+      <?php 
             $connection = mysqli_connect('52.78.0.158','remoteJO','remoteJO','happyTogether',56946);        
 
             $main_category_query = "select main_category_name from main_category";
@@ -65,15 +76,15 @@
             $main_category = mysqli_fetch_array($main_category_result);
 
         ?>
-        <p class='mainmessage'>
+      <p class='mainmessage'>
         서브 카테고리 추가하기
-        </p>
+      </p>
 
-        <form name="form" method="post" action="insert_sub_category.php" class='container'>
+      <form name="form" method="post" action="insert_sub_category.php" class='container'>
         <span class='message'>1. 메인 카테고리 선택</span>
         <select name="main_category" style="width: 100px ;">
-        <option >선택</option>
-        <?php
+          <option>선택</option>
+          <?php
             for($i=0; $i < $main_category_count; $i++){
                 echo "<option value= $main_category[0]>$main_category[0]</option>";
                 $main_category = mysqli_fetch_array($main_category_result);
@@ -83,10 +94,13 @@
         <span class='message'>2. 서브 카테고리 입력 </span>
         <input type="text" name="sub_category" class='textarea' />
         <div class="btns">
-            <input type="submit" value="입력"  class='button' />
-            <input type="button" value="닫기" class="button close" />
+          <input type="submit" value="입력" class='button' />
+          <input type="button" value="닫기" class="button close" />
         </div>
-        </form>
+      </form>
     </body>
-    <html></html>
+    <html>
+
     </html>
+
+  </html>
